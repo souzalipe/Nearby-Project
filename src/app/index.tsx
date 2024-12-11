6871827b3bebe039920fba97a30a63e0d508cdfa
Felipe Nascimento de Souza <fn23886@gmail.com>
2024-12-10 22:44:55 -0300

    feat: index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/button"
 
 export default function Index() {
     return(
-        <View style={{ flex:1, padding: 40, gap:40  }}>
+        <View style={{ flex:1, padding: 40, gap: 40  }}>
             <Welcome />
             <Steps/>
 
